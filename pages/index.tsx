f2762aa6d11a63fe2c0971deda7d49efd2877c8f
Ali Nazari <ali.nazari@aut.ac.ir>
2019-12-19 18:56:22 +0330

    project switched to typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from 'antd'
 import "../styles/index.scss"
+import "antd/dist/antd.css";
 
 function Home() {
   return(
     <div className="root">
-      
+      <Button className="a" type="primary">Hi</Button>
     </div>
   )
 }
