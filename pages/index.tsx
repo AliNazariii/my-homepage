8491f21b8e518988b0c14c738c009da71a039590
Ali Nazari <ali.nazari@aut.ac.ir>
2019-12-29 18:52:44 +0330

    add tag for projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,20 +72,35 @@ function Home() {
               <h2 className="card-title">Projects</h2>
               <p className="card-paragraph">
                 <div className="card-item">
-                  <Icon className="card-item" type="code" theme="filled" /> <b><a href="https://ali-nazari.ir/">My Homepage</a></b> (Winter 2019)
+                  <Icon className="card-item" type="code" theme="filled" /> <b><a href="https://ali-nazari.ir/">My Homepage</a></b> (Winter 2019) &emsp;
                   <br /> I developed this website as a homepage for myself in my free time.
                   <br /> <a href="https://github.com/AliNazariii/my-homepage">Here</a> you can see my repository for this project.
+                  <br />
+                  <Card className="tech-item"><p className="tech-item-text">React</p></Card>
+                  <Card className="tech-item"><p className="tech-item-text">Next.JS</p></Card>
+                  <Card className="tech-item"><p className="tech-item-text">Typescript</p></Card>
+                  <Card className="tech-item"><p className="tech-item-text">Ant Design</p></Card>
+                  <Card className="tech-item"><p className="tech-item-text">SASS</p></Card>
+                  <Card className="tech-item"><p className="tech-item-text">CSS</p></Card>
                 </div>
                 <div className="card-item">
                   <Icon className="card-item" type="code" theme="filled" /> <b><a href="https://icpc.aut.ac.ir/">AUT ICPC Website</a></b> (Summer 2019)
                   <br /> I started front-end of this project with <a href="https://www.linkedin.com/in/keshavarz13/">MohamadAli Keshavarz</a>.
                   <br /> <a href="https://github.com/aut-icpc/ACM-frontend">Here</a> is some more information about our development process.
+                  <br />
+                  <Card className="tech-item"><p className="tech-item-text">React</p></Card>
+                  <Card className="tech-item"><p className="tech-item-text">Material-UI</p></Card>
+                  <Card className="tech-item"><p className="tech-item-text">CSS</p></Card>
+                  <Card className="tech-item"><p className="tech-item-text">Team-Work</p></Card>
                 </div>
                 <div className="card-item">
                   <Icon className="card-item" type="code" theme="filled" /> <b><a href="https://github.com/AliNazariii/Elevator">Elevator</a></b> (Autumn 2018)
                   <br /> It was the final project of our Logical Cicuit course at AUT. 
-                  <br />First, we think as a group to design an efficient FSM (finite state machine) for movement and also the management of the elevator. Finally, we implement it in Verilog.
+                  <br />First, we think as a group to design an efficient FSM (finite state machine) for movement and also the management of the elevator.
                   <br /> <a href="https://github.com/AliNazariii/Elevator">Here</a> is some more information about it.
+                  <br />
+                  <Card className="tech-item"><p className="tech-item-text">Verilog</p></Card>
+                  <Card className="tech-item"><p className="tech-item-text">FSM</p></Card>
                 </div>
                 <div className="card-item">
                   <Icon className="card-item" type="code" theme="filled" /> <b><a href="https://github.com/AliNazariii/progviz">ProGViz</a></b> (Summer 2018)
@@ -93,27 +108,44 @@ function Home() {
                   <br /> ProGViz is a cross-platform graph-visualization desktop-application for visualizing well-known graphical representations of computer program source code such as CFGs, PDGs, ASTs, etc.
                   <br /> It was an internal project by a group of students of Amirkabir University of Technology, as a sub-project of the PhD thesis of <a href="http://linkedin.com/in/smghaffarian">Seyed Mohammad Ghaffarian</a>. 
                   <br /> <a href="https://github.com/AliNazariii/progviz">Here</a> is some more information about our development process.
+                  <br />
+                  <Card className="tech-item"><p className="tech-item-text">HTML</p></Card>
+                  <Card className="tech-item"><p className="tech-item-text">CSS</p></Card>
+                  <Card className="tech-item"><p className="tech-item-text">JavaScript</p></Card>
+                  <Card className="tech-item"><p className="tech-item-text">Electron.JS</p></Card>
                 </div>
                 <div className="card-item">
                   <Icon className="card-item" type="code" theme="filled" /> <b><a href="https://github.com/AliNazariii/Game-Of-Tanks">Game of Tanks</a></b> (Summer 2018)
                   <br /> A 2D java game containing both single player & multi player modes. Player spawns with a tank and its mission is to eliminate all enemies on the map.
                   <br /> The project is based on Buffer-Strategy implementation. It was our Advanced Programming final project and I implemented it with <a href="https://github.com/Amirhossein-Hediehloo">AmirHosein Hediehloo</a>.
                   <br /> <a href="https://github.com/AliNazariii/Game-Of-Tanks">Here</a> is some more information about our development process.
+                  <br />
+                  <Card className="tech-item"><p className="tech-item-text">Java</p></Card>
+                  <Card className="tech-item"><p className="tech-item-text">Swing</p></Card>
+                  <Card className="tech-item"><p className="tech-item-text">Team-Work</p></Card>
                 </div>
                 <div className="card-item">
                   <Icon className="card-item" type="code" theme="filled" /> <b><a href="https://github.com/AliNazariii/Download-Manager">JDM (Java Download Manager)</a></b> (Spring 2018)
                   <br /> I started front-end of this project with <a href="https://www.linkedin.com/in/keshavarz13/">MohamadAli Keshavarz</a>. We developed it by ReactJS.
                   <br /> <a href="https://github.com/AliNazariii/Download-Manager">Here</a> is some more information about our development process.
+                  <br />
+                  <Card className="tech-item"><p className="tech-item-text">Java</p></Card>
+                  <Card className="tech-item"><p className="tech-item-text">Swing</p></Card>
+                  <Card className="tech-item"><p className="tech-item-text">Network</p></Card>
                 </div>
                 <div className="card-item">
                   <Icon className="card-item" type="code" theme="filled" /> <b><a href="https://github.com/AliNazariii/Battle-Ship-Game">Battle Ship</a></b> (Spring 2018)
                   <br /> I started front-end of this project with <a href="https://www.linkedin.com/in/keshavarz13/">MohamadAli Keshavarz</a>. We developed it by ReactJS.
                   <br /> <a href="https://github.com/AliNazariii/Battle-Ship-Game">Here</a> is some more information about our development process.
+                  <br />
+                  <Card className="tech-item"><p className="tech-item-text">Java</p></Card>
                 </div>
                 <div className="card-item">
                   <Icon className="card-item" type="code" theme="filled" /> <b><a href="https://github.com/AliNazariii/Hangman-Game">Hangman</a></b> (Autumn 2017)
                   <br /> I started front-end of this project with <a href="https://www.linkedin.com/in/keshavarz13/">MohamadAli Keshavarz</a>. We developed it by ReactJS.
                   <br /> <a href="https://github.com/AliNazariii/Hangman-Game">Here</a> is some more information about our development process.
+                  <br />
+                  <Card className="tech-item"><p className="tech-item-text" style={{marginLeft: "-5px"}}>C</p></Card>
                 </div>
               </p>
             </Card>
