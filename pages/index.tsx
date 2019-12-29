59b242377553a115761501009fd3750209744276
Ali Nazari <ali.nazari@aut.ac.ir>
2019-12-29 19:15:56 +0330

    add achievement part

--- pages/index.tsx
+++ pages/index.tsx
@@ -149,12 +149,6 @@ function Home() {
                 </div>
               </p>
             </Card>
-            <Card className="card">
-              <h2 className="card-title">Achievements</h2>
-              <p className="card-paragraph">
-                I am Ali Nazari. A 21 years old student
-              </p>
-            </Card>
           </div>
           <div className="right-side">
             <Card className="card">
@@ -215,6 +209,20 @@ function Home() {
                 </div>
                 <div className="card-item">
                   Also Familiar with Arabic
+                </div>
+              </p>
+            </Card>
+            <Card className="card">
+              <h2 className="card-title">Achievements</h2>
+              <p className="card-paragraph">
+                <div className="card-item">
+                  <Icon type="trophy" theme="filled" />
+                  &nbsp;Achieved top 0.3% place among all applicants of the Nationwide University Entrance Exam for B.Sc. in Math. 
+                  and Engineering (Approximately 137000 applicants), Iran - July 2017.
+                </div>
+                <div className="card-item">
+                  <Icon type="trophy" theme="filled" />
+                  &nbsp;Accepted in first stage of Chemistry Olympiad in National Organization for Development of Exceptional Talents, Iran - June 2015.
                 </div>
               </p>
             </Card>
