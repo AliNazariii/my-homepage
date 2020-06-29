a8e832933b28befd5ac5d997cdf652b2dac3194b
Ali Nazari <ali.nazari@aut.ac.ir>
2020-06-29 14:37:30 +0430

    ready to release v2.0

--- components/interests.tsx
+++ components/interests.tsx
@@ -6,10 +6,10 @@ function Interests() {
       <Card className="card">
         <h2 className="card-title">Interests</h2>
         <p className="card-paragraph">
-          <BookFilled className="card-item icons-in-cards" type="book" theme="filled" /> Front-End Technologies
-          <br /><BookFilled className="card-item icons-in-cards" type="book" theme="filled" /> Graphic Design
-          <br /><BookFilled className="card-item icons-in-cards" type="book" theme="filled" /> Psychology
-          <br /><BookFilled className="card-item icons-in-cards" type="book" theme="filled" /> Teaching
+          <BookFilled className="card-item icons-in-cards" /> Front-End Technologies
+          <br /><BookFilled className="card-item icons-in-cards" /> Graphic Design
+          <br /><BookFilled className="card-item icons-in-cards" /> Psychology
+          <br /><BookFilled className="card-item icons-in-cards" /> Teaching
         </p>
       </Card>
     )
